ac05643e35fc0d88c62aa70604d4c63908d17605
Mojo <mhamad.alshame2134@gmail.com>
2024-07-25 22:19:33 +0400

    ss

--- lib/Veiw/Splach_screen/component_splach.dart
+++ lib/Veiw/Splach_screen/component_splach.dart
@@ -9,18 +9,20 @@ class Component extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(decoration:BoxDecoration(borderRadius:BorderRadius.circular(55),
-          color:Colors.black87,
-        ),
-          alignment: Alignment.center,
-          margin: const EdgeInsets.only(left: 140, top: 140),
-          // padding: EdgeInsets.only(left: 10, top: 10),
-          height: 120,
-          width: 120,
-          child:Text('FIXIT',style:kTitleTextStyle,),),
-      ],
+    return Center(
+      child: Column(
+        children: [
+          Container(decoration:BoxDecoration(borderRadius:BorderRadius.circular(55),
+            color:Colors.black87,
+          ),
+            alignment: Alignment.center,
+            margin: const EdgeInsets.only(left: 140, top: 140),
+            // padding: EdgeInsets.only(left: 10, top: 10),
+            height: 120,
+            width: 120,
+            child:Text('FIXIT',style:kTitleTextStyle,),),
+        ],
+      ),
     );
   }
 }
